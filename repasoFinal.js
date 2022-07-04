6d27ea79cbcba1941e9b94e089a549c27f6e4f7d
JulianR <julian.russo@team.wirall.com>
2022-07-04 16:13:23 -0300

    repaso final

--- repasoFinal.js
+++ repasoFinal.js
@@ -3,23 +3,49 @@
 
 // ORDENAR LOS ESTUDIANTES POR LA EDAD , DE FORMA ASCENDENETE
 let estudiantes = [
-    {
-      nombre: "zaul",
-      edad: 12,
-    },
-    {
-      nombre: "antonella",
-      edad: 22,
-    },
-    {
-      nombre: "teresa",
-      edad: 42,
-    },
+  {
+    nombre: "teresa",
+    edad: 42,
+  },
+  {
+    nombre: "zaul",
+    edad: 12,
+  },
+  {
+    nombre: "antonella",
+    edad: 22,
+  },
 ];
 
+const ordenar = arr => {
+
+  let aux;
+
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr.length - 1; j++) {
+      
+      if( arr[j].edad > arr[j + 1].edad ){
+
+        aux = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = aux
+
+      }
+      
+    }
+    
+  }
+
+}
+
+ordenar(estudiantes)
+console.log( estudiantes )
 
 
-// RECORRER PRINCIPAL Y MULTIPLICAR LOS IMPARES
+
+// MULTIPLICAR LOS ELEMENTOS DE LA DIAGONAL PRINCIPAL
+// PERO SOLO LOS MULTIPLOS DE 5
 
 let matriz = [
 
@@ -27,6 +53,24 @@ let matriz = [
     [9, 15, 27],
     [29, 11, 14],
 ];
+
+const principal = mat => {
+
+  let acc = 1
+
+  for (let i = 0; i < mat.length; i++) {
+    
+    if( mat[i][i] % 5 === 0 ){
+
+      acc *= mat[i][i]
+
+    }
+
+  }
+
+  return acc
+
+}
 
 
 // RECORRER LA COLUMNA DE LA POSICION 0 Y SUMAR LOS 
@@ -36,15 +80,87 @@ let matriz2 = [
 
     [25, 31],
     [9, 27],
-    [29, 14],
-    [59, 16],
+    [30, 14],
+    [58, 16],
 ];
 
+const recorrerColCero = mat =>{
+
+  let acc = 0
+  for (let i = 0; i < array.length; i++) {
+    
+    if( mat[i][0] % 2 === 0 && mat[i][0] > 15 ){
+
+      acc += mat[i][0]
+
+    }
+    
+  }
+
+  return acc
+
+}
 
 
 // ORDENAR EL SIGUIENTE ARREGLO DE FORMA DESCENDENTE 
-// SI RECIBIMOS POR PARAMETROS LA INDICACION,
+// UNICAMENTE SI RECIBIMOS POR PARAMETROS LA INDICACION,
 // SI NO, DEJAREMOS EL ALGORITMO LISTO PARA LA PROXIMA
 
-let numeros = [ 522, 122, 2, 155, 741] // arrJ < arrJ+1  / arrJ = 2 / arrJ+1 = 122
+let numeros = [ 522, 122, 2, 155, 741]
 
+const ordenarDescendente = ( arr, indicacion )=>{
+
+  if( indicacion === undefined ){
+    console.log( "Lo siento, no recibi la indicacion" )
+    return 
+  }
+
+  let temp;
+
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr.length - 1; j++) {
+      
+      if( arr[j] < arr[j + 1] ){
+
+        temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+
+      }
+      
+    }
+    
+  }
+
+}
+
+ordenarDescendente(numeros)
+console.log( numeros)
+
+
+// SUMAR LOS ELEMENTOS IMPARES DE LA DIAGONAL SECUNDARIA
+
+let matriz3 = [
+
+  [25, 31, 12],
+  [10, 27, 13],
+  [10, 27, 21],
+
+];
+
+const secundaria = mat =>{
+
+  let acc = 0
+
+  for (let i = 0; i < mat.length; i++) {
+   
+    if( mat[i][mat.length - 1 - i] % 2 !== 0 ){
+
+      acc += mat[i][mat.length - 1 - i]
+
+    }
+    
+  }
+
+}
