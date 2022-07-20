3c559a3189579087fc242e74fff4c3dff8a1c9a7
jorgeduje <julian.russo@team.wirall.com>
2022-07-20 15:09:53 -0300

    cambios

--- metodosArray.js
+++ metodosArray.js
@@ -27,12 +27,7 @@ console.log( arrayMultX3 )
 let arrayMultiplicado = numeros.map( x => x * 3)
 console.log( arrayMultiplicado )
 
-// .map / .filter / .reduce / .forEach / .find / .sort
-
-// .map()
-//.filter()
-
-
+// .map / .filter / .reduce / .forEach / .find /
 
 const filtar = arr => {
     let arrNuevo = []
@@ -49,7 +44,7 @@ const filtar = arr => {
 }
 
 console.log( filtar(numeros) )
-let numerosFiltrados =  numeros.filter( (elemento, indice) => elemento < 4) // []
+let numerosFiltrados =  numeros.filter( (elemento, indice) => elemento < 4)
 
 
 
@@ -87,58 +82,133 @@ let productos = [
         nombre: "zapatilla",
         precio: 15000,
         color: "blanco"
-    },
-    {
-        nombre: "vw",
-        color: "morado",
-        precio: 3000000
     }
 ]
 
-let fueFiltrado = false // STATE FLAG O ESTADOS BANDERA
 
-let filtrarPorPrecio = ()=>{
-    
-    let filtrados = productos.filter( producto => producto.precio <= 40000)
-    fueFiltrado = true
-    return filtrados
+// .reduce / .forEach / .sort() /
+
+// FIND = RETURNAR EL ELEMENTO TAL CUAL COMO ES
+
+let filtrado = productos.filter( ( elemento )=> elemento.nombre === "televisor"  )
+let encontrado = productos.find( ( elemento )=> elemento.nombre === "televisor"  )
+console.log(filtrado)
+console.log(encontrado)
+
+// .forEach 
+
+// let num = [1, 2, 4, 5]
+let productos2 = [
+    {
+        nombre: "televisor",
+        precio: 50000,
+        color: "negro"
+    },
+    {
+        nombre: "celular",
+        precio: 40000,
+        color: "rojo"
+    },
+    {
+        nombre: "zapatilla",
+        precio: 15000,
+        color: "blanco"
+    }
+]
+
+productos2.forEach( (elemento, indice)=> {
+    elemento.id = indice + 1
+} )
+
+console.log(productos2)
+
+let num = [ 1, 2, 3]
+
+let num2 = []
+
+num.map( elemento => {
+    num2.push( elemento * 2)
+})
+
+console.log(num2)
+
+
+let palabras = [ "zebra", "aguila", "jirafa" ]
+
+let numeritos = [ 12, 4, 110, 1, 22, 333]
+
+console.log( palabras )
+
+console.log( palabras.sort() )
+
+console.log(numeritos)
+console.log( palabras.sort( (a, b) => a - b ) )
+
+let index = 2
+// console.log(array1.at(index))
+
+// .reduce
+
+// LLEVA UN ARREGLO A SU MAXIMA REDUCCION NUMERO O UN STRING
+
+let numeros10 = [ 1, 4, 6 ]
+
+// PRIMER PARAMETRO RECIBE EL ACC y SEGUNDO EL ELEMENTO
+console.log( numeros10.reduce( (acc, elemento)=>{
+
+    return acc * elemento
+
+} ,) )
+
+let productos3 = [
+    {
+        nombre: "televisor",
+        precio: 50000,
+        color: "negro"
+    },
+    {
+        nombre: "celular",
+        precio: 40000,
+        color: "rojo"
+    },
+    {
+        nombre: "zapatilla",
+        precio: 15000,
+        color: "blanco"
+    }
+]
+
+console.log( productos3.reduce( (acc, producto)=>{
+
+    return acc + producto.precio
+
+}, ))
+
+let numerosss = [ 12, 41321, 1231, 555, 123, 1235, ]
+
+let ordenados = numerosss.sort( (a, b) => a - b)
+
+let numeroMayor = ordenados[ ordenados.length - 1]
+
+console.log(numeroMayor )
+
+console.log(numerosss.reduce((acc, elemento)=> acc > elemento ? acc : elemento ))
+
+console.log( Math.min( ...numerosss ) )
+
+
+// DESESTRUCTURACION DE OBJETOS
+
+let persona = {
+
+    nombre: "juancito",
+    edad: 29,
+
 }
 
-const AgregarAlCarrito = (indice)=>{
+// VERBOS HTTP
 
-    "AGREGUE AL CARRITO EL ELEMETO CON EL ID PASADO POR PARAMETRO"
-}
-
-<>
-    <button onClick={filtrarPorPrecio}>FILTRAR</button>
-
-    <div class="container">
-
-
-        {
-            fueFiltrado === false ? productos.map( (producto, indice) => {
-                return (
-                    <div>
-                        <h2>{producto.nombre}</h2>
-                        <h2>{producto.color}</h2>
-                        <h2>{producto.precio}</h2>
-                        <button onClick={()=>AgregarAlCarrito(indice)}>Comprar</button>
-                    </div>
-                )
-            }) : filtrados.map( (producto) => {
-                return (
-                    <div>
-                        <h2>{producto.nombre}</h2>
-                        <h2>{producto.color}</h2>
-                        <h2>{producto.precio}</h2>
-                        <button onClick={AgregarAlCarrito}>Comprar</button>
-                    </div>
-                )
-            })
-        }
-
-    </div>
-</>
-
-algo ? "true" : "falso"
-
+// GET(/usuarios) 
+// POST(/usarios, data) 
+// PUT (/usaruis, data)
+// DELETE(/usuarios, id)
